3628cf5fe19be5ed376054bef770bf32f81202bc
Cleber Lucas <clebersiber@hotmail.com>
2024-04-02 12:29:53 -0300

    adding registration option on the website

--- Server/facialDataAccessLayer.py
+++ Server/facialDataAccessLayer.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import mysql.connector
 from mysql.connector import Error
 import json
@@ -9,6 +10,7 @@ with open('config.json') as f:
     config = json.load(f)
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/esp/start', methods=['POST'])
 def send_ip():
@@ -194,5 +196,48 @@ def get_esp_register_mode():
     except mysql.connector.Error as err:
         return jsonify({'message': 'Error executing SQL query: ' + str(err)}), 500
 
+@app.route('/register-user', methods=['POST'])
+def register_user():
+    try:
+        id = request.json['id']
+        name = request.json['name']
+        local = request.json['local']
+
+        if not id:
+            return jsonify({'message': 'ID parameter is missing'}), 400
+
+        if not name:
+            return jsonify({'message': 'Name parameter is missing'}), 400
+
+        if not local:
+            return jsonify({'message': 'Local parameter is missing'}), 400
+
+        db_config = config.get("db", {})
+        db_connection = mysql.connector.connect(
+            host=db_config.get("host", "localhost"),
+            user=db_config.get("user", "root"),
+            password=db_config.get("password", ""),
+            database=db_config.get("database", "")
+        )
+
+        if db_connection.is_connected():
+            cursor = db_connection.cursor()
+
+            cursor.execute("REPLACE INTO user (id, name, local) VALUES (%s, %s, %s)", (id, name, local))
+            db_connection.commit()
+
+            cursor.close()
+            db_connection.close()
+
+            return jsonify({'message': 'User registered successfully'}), 201
+        else:
+            return jsonify({'message': 'Database connection failed'}), 500
+
+    except Error as e:
+        return jsonify({'message': 'An error occurred: ' + str(e)}), 500
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+
+
